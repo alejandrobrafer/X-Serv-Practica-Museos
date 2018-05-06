d340e1dd032ac11cadc9078ca6bebc6292560f2d
Alejandro  Bravo Fernández <alejandrobravofernandez@MacBook-Pro-de-Alejandro.local>
2018-05-06 03:44:05 +0200

    añadido login

--- practica/museos/views.py
+++ practica/museos/views.py
@@ -9,15 +9,23 @@ from django.contrib.auth.models import User
 from django.db.models import Count
 from django.contrib import auth
 
+registration_form = '''
+<form method="POST" action="/login">
+<p><label>Usuario
+<input type="username" class="fields" name='username' />
+</label>
+<label>Contraseña
+<input type="password" class="fields" name='password' />
+<input type="submit" class="submit_button" name="Submit" value="Entrar" />
+</label></p>
+</form>
+'''
 
 @csrf_exempt
 def home(request):
-	if request.method == 'GET' or request.method == 'POST':
+	if request.method == 'GET':
+		user_login = request.user
 		# Nota: Abra una parte con el filtrado del XML de Museos de Madrid.
-
-#		if request.user.is_authenticated():
-#			print(request.user.username)
-
 		museums2show = None
 		string = ""
 		# Obtención de la Query String: https://docs.djangoproject.com/en/1.8/ref/request-response/
@@ -47,7 +55,7 @@ def home(request):
 				title = "Página de " + username
 			personal_pages += "Título: <a href='/" + username + "'>" + title + "</a> Usuario: " + username + "<br/><br/>"
 		
-		return render_to_response('home.html', {'commented_museums': commented_museums, 'personal_pages': personal_pages, 
+		return render_to_response('index.html', {'user': user_login, 'commented_museums': commented_museums, 'personal_pages': personal_pages, 
 												'str': string, 'button': button, 'museums2show': museums2show})
 	else:
 		response = "Method not allowed"
@@ -56,7 +64,12 @@ def home(request):
 @csrf_exempt
 def user(request, name):
 	if request.method == 'GET':	
+		user_login = request.user
 		# NOTA: QUE PASA SI EL MISMO USUARIO SELECCIONA EL MISMO MUSEO 2 O MAS VECES <----> Idea en Notas
+		
+		if request.user.is_authenticated():
+			print("HOLA")
+
 		try:
 			user = User.objects.get(username = name)
 		except User.DoesNotExist:
@@ -81,7 +94,7 @@ def user(request, name):
 			qs += 1
 			button = "<a href='/" + name + "?" + str(qs) + "'>" + "<button> Ver más...</button>" + "</a><br>"
 
-		return render_to_response('user.html', {'museums': museums, 'name': name, 'button': button})
+		return render_to_response('user.html', {'user': user_login, 'museums': museums, 'name': name, 'button': button})
 	else:
 		response = "Method not allowed"
 		return HttpResponse(response, status = 405)
@@ -89,7 +102,7 @@ def user(request, name):
 @csrf_exempt
 def museums(request):
 	# NOTA: supongo que aqui es donde se añadiran los museos a un usuario registrado
-
+	user_login = request.user
 	# Es necesario sacar una lista de los distritos para pasarlo al formulario
 	# Con el list(set()) lo que obtengo es los valores no repetidos de una lista
 	# value_list: https://docs.djangoproject.com/en/2.0/ref/models/querysets/
@@ -99,14 +112,14 @@ def museums(request):
 
 	if request.method == 'GET':
 		museums = Museums.objects.all()
-		return render_to_response('museums.html', {'museums': museums, 'districts': districts}) 
+		return render_to_response('museums.html', {'user': user, 'museums': museums, 'districts': districts}) 
 	elif request.method == 'POST':
 		dm = request.POST['Option']
 		if dm == "Todos":
 			museums = Museums.objects.all()
 		else:
 			museums = Museums.objects.filter(District = dm)
-		return render_to_response('museums.html', {'museums': museums, 'districts': districts})  
+		return render_to_response('museums.html', {'user': user_login, 'museums': museums, 'districts': districts})  
 	else:
 		response = "Method not allowed"
 		return HttpResponse(response, status = 405)
@@ -114,10 +127,11 @@ def museums(request):
 
 def museum_page(request, id):
 	if request.method == 'GET':
+		user_login = request.user
 		try:
 			museum = Museums.objects.get(id = id)
 			comments = Comments.objects.filter(Museum = museum)
-			return render_to_response('museum_page.html', {'museum': museum, 'comments': comments})  
+			return render_to_response('museum_page.html', {'user': user_login, 'museum': museum, 'comments': comments})  
 		except Museums.DoesNotExist:
 			response = "Page not found"
 			return HttpResponseNotFound(response)
@@ -146,19 +160,16 @@ def about(request):
 	#NOTA: Falta por hacer
 	return HttpResponse("About")
 
+
 @csrf_exempt
 # http://librosweb.es/libro/django_1_0/capitulo_12/utilizando_usuarios.html --> parte opcional de registro tambien
 def login(request):
-	print(request)
 	if request.method == 'POST':
 		username = request.POST['username']
-		print(username)
 		password = request.POST['password']
-		print(password)
 		user = auth.authenticate(username = username, password = password)
 		if user is not None and user.is_active:
 			auth.login(request, user)
-			print("Correct")
 			return HttpResponseRedirect("/")
 	else:
 		response = "Method not allowed"
